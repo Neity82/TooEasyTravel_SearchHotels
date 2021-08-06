6711e60ce5a8a0c0ae071b4058df80b8b9d4cc77
Neity <nata_111082@mail.ru>
2021-08-06 20:32:54 +0300

    First stage

---
+++ Bot/botrequests/hotel_class.py
@@ -0,0 +1,76 @@
+from typing import Dict
+
+from emoji import emojize
+
+from Bot.log import logging_decor_cls
+
+
+@logging_decor_cls
+class Hotel:
+    """
+    Класс Hotel
+
+    Args:
+        all_info (Dict): передается  полная информация об отеле
+
+    Attributes:
+        _all_info(Dict): полная информация об отеле
+        _name (str): название отеля
+        _stars (int): количество звезд
+        _rating (str): рейтинг
+        _address (str): адрес
+        _distance (str): расстояние до центра
+        _price (str): цена за 1 ночь
+    """
+
+    def __init__(self, all_info: Dict) -> None:
+        self._all_info = all_info
+        self._name = self._all_info.get("name", '')
+        self._stars = int(self._all_info.get("starRating"))
+        self._rating = self._all_info.get("guestReviews", {}).get("rating", '')
+        self._address = ', '.join([i_value for i_key, i_value in self._all_info.get("address", {}).items()
+                                   if i_key in ["streetAddress", "locality", "countryName"]])
+        self._distance = self._all_info.get("landmarks", [{}])[0].get("distance", '')
+        self._price = self._all_info.get("ratePlan", {}).get("price", {}).get("current", '')
+
+    def __str__(self) -> str:
+        return "*{name}*\n{stars}\n{address}\nРасстояние до центра: {distance}\n " \
+               "Рейтинг: *{rating}*\nЦена за 1 ночь: *{price}*".format(stars=emojize(":star:") * self._stars,
+                                                                       name=self._name,
+                                                                       address=self._address,
+                                                                       distance=self._distance,
+                                                                       rating=self._rating,
+                                                                       price=self._price)
+
+    @property
+    def all_info(self):
+        return self._all_info
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def starts(self):
+        return self._stars
+
+    @property
+    def rating(self):
+        return self._rating
+
+    @property
+    def address(self):
+        return self._address
+
+    @property
+    def distance(self):
+        return self._distance
+
+    @property
+    def price(self):
+        return self._price
+
+    @all_info.setter
+    def all_info(self, info):
+        self._all_info = info
+
---
+++ Bot/log.py
@@ -0,0 +1,45 @@
+import os
+import functools
+from typing import Callable
+
+from loguru import logger
+
+path_log: str = os.sep.join(("logs", "logging_{time}.log"))
+logger.add(path_log, format="{time} | {level}   | {message}", level="DEBUG", encoding="utf-8")
+
+
+def logging_decor(func: Callable) -> Callable:
+    """
+    Функция декоратор, записывает в файл 'logging.log' о вызове функции.
+    Передает в файл название функции и аргументы
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        logger.debug("Вызвана функция: {func}, аргументы: {args}, {kwargs}".format(func=func.__name__,
+                                                                                   args=args, kwargs=kwargs))
+        for i_arg in args:
+            logger.info("Детализация: {info}".format(info=i_arg))
+
+        result = func(*args, **kwargs)
+        return result
+
+    return wrapper
+
+
+def logging_decor_cls(cls):
+    """
+    Функция декоратор класса, записывает в файл 'logging.log' о создании инстанса класса.
+    Передает в файл название класса и переданные аргументы
+    """
+
+    @functools.wraps(cls)
+    def wrapper(*args, **kwargs):
+        logger.debug("Создан инстанс класса: {cls}, аргументы: {args}, {kwargs}".format(cls=cls.__name__,
+                                                                                        args=args, kwargs=kwargs))
+        instance = cls(*args, **kwargs)
+        return instance
+
+    return wrapper
+
+
